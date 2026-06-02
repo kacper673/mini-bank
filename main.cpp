a694d959dcaeb12e0ea213826edfb73a4d5bea66
kacper673 <albertrun112020@gmail.com>
2026-06-02 20:30:10 +0200

    Added alternative console version

--- main.cpp
+++ main.cpp
@@ -4,7 +4,6 @@
 int main(int argc, char* argv[]) {
     QApplication app(argc, argv);
 
-    // Fusion - czysty, nowoczesny wyglad identyczny na Windows/Linux/Mac
     app.setStyle("Fusion");
 
     MainWindow window;
